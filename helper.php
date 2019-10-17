04fd9aa8fbc206b4bf2008a2e732016c0c4b6de7
Thomas Winterling <info@winterling-labs.com>
2019-10-17 16:53:16 +0200

    Create Typed Module Helper

---
+++ helper.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_banners
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_banners
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_banners
+ * @since       1.5
+ */
+<?php
+/**
+ * @package    TYPED MODUL
+ *
+ * @author     Thomas Winterling <info@winterling-labs.com>
+ * @copyright  Copyright (C) 2005 - 2019. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_dd_gmaps_module
+ *
+ * @since  Version 1.0.0.0
+ */
+class Mod_Typed_Module_Helper
+{
+	
+}
