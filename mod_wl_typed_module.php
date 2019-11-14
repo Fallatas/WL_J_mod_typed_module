9e747f71eed6771d2fe5b180120b41efe73a1623
Thomas Winterling <info@winterling-labs.com>
2019-11-14 14:51:25 +0100

    joomla 4 namesspace

--- mod_wl_typed_module.php
+++ mod_wl_typed_module.php
@@ -9,28 +9,33 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Helper\ModuleHelper;
+
 // Include the syndicate functions only once
 require_once __DIR__ . '/helper.php';   // Helper
 
-JHTML::_('script', 'mod_wl_typed_module/scripts.js', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('script', 'mod_wl_typed_module/scripts.js', array('version' => 'auto', 'relative' => true));
 
 $data = modWL_Typed_Module_Helper::getTypedParams ($params);
 
-$css = modWL_Typed_Module_Helper::setCssParams ($params);
+modWL_Typed_Module_Helper::setCssParams ($params);
 
-$js = modWL_Typed_Module_Helper::SetJsParams ($data);
+modWL_Typed_Module_Helper::SetJsParams ($data);
 
 if($data->words == ""){
 
     // Get a handle to the Joomla! application object
-    $application = JFactory::getApplication();
+    $application = Factory::getApplication();
 
     // Add a message to the message queue
-    $application->enqueueMessage(JText::_('MOD_WL_TYPED_MODULE_PLACEHOLDER'), 'Warning');
+    $application->enqueueMessage(Text::_('MOD_WL_TYPED_MODULE_PLACEHOLDER'), 'Warning');
 }
 
 	// Check for a custom CSS file
-    JHtml::_('stylesheet', 'mod_wl_typed_module/user.css', array('version' => 'auto', 'relative' => true));
+    HTMLHelper::_('stylesheet', 'mod_wl_typed_module/user.css', array('version' => 'auto', 'relative' => true));
     
     
-   require JModuleHelper::getLayoutPath('mod_wl_typed_module', $params->get('layout', 'default'));
+   require ModuleHelper::getLayoutPath('mod_wl_typed_module', $params->get('layout', 'default'));
